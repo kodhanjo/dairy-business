4bd11495671055c19d5b7d49dc452335b9969006
kodhanjo <felix.osambo@student.moringaschool.com>
2021-05-03 15:31:02 +0300

    production

---
+++ app.js
@@ -0,0 +1,25 @@
+const production = {
+    shadeA: {
+        numberOfCows: 34,
+        avarageProduction: 15,
+    },
+    shadeB: {
+        numberOfCows: 11,
+        avarageProduction: 28,
+    },
+    shadeC: {
+        numberOfCows: 27,
+        avarageProduction: 18,
+    },
+    shadeD: {
+        numberOfCows: 52,
+        avarageProduction: 11,
+    },
+
+}
+const totalProduction ={
+    milkPrice = 45,
+    monthS = ["Jan", "Feb", "Mar","April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec" ]
+};
+
+console.log(totalProduction);
